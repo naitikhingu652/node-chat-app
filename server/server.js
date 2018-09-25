adefc3e62947144894def774517e5219e49d7ec4
HINGU <naitik.hingu@team.telstra.com>
2018-09-25 15:54:14 +0530

    Add newMessage and createMessage event

--- server/server.js
+++ server/server.js
@@ -14,6 +14,16 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
     console.log("New user connected");
 
+    socket.emit('newMessage', {
+        from: 'Aniket',
+        text: 'Hey, How are you?',
+        createdAt: 123
+    });
+
+    socket.on('createMessage', (message) => {
+        console.log("Create Message", message);
+    });
+
     socket.on('disconnect', () => {
         console.log('Disconnected from server');
     });
