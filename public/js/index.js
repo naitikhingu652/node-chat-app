389d116d5b53fec513e702b97b4e5c02bd0d2ed2
HINGU <naitik.hingu@team.telstra.com>
2018-09-26 10:27:07 +0530

    Emit newMessage on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -2,11 +2,6 @@ var socket = io();
 
 socket.on('connect', function () {
     console.log('Connected to server');
-
-    socket.emit('createMessage', {
-        from: 'Naitik',
-        text: 'Hey, I am good.'
-    });
 });
 
 socket.on('disconnect', function () {
