9464249b2042800528a78d4739bb0e2c67f5bcb9
HINGU <naitik.hingu@team.telstra.com>
2018-09-26 14:04:11 +0530

    Add form for messages and show incoming messages in browser

--- public/js/index.js
+++ public/js/index.js
@@ -10,4 +10,19 @@ socket.on('disconnect', function () {
 
 socket.on('newMessage', function(message) {
     console.log('New Message', message);
+    let li = $('<li></li>');
+    li.text(`${message.from}: ${message.text}`);
+
+    $('#messages').append(li);
+});
+
+$('#message-form').on('submit', function(e) {
+    e.preventDefault();
+
+    socket.emit('createMessage', {
+        from: 'Naitik',
+        text: $('[name=message]').val()
+    }, function(data) {
+        console.log('Got it.', data);
+    });
 });
